64bb2d17c754d5fb81477689420a8d65cfceb77a
GuanTony <guantony33@gmail.com>
2022-12-02 06:29:42 +0100

    version final

--- index.js
+++ index.js
@@ -10,99 +10,67 @@ const client = new Client({
   ],
 });
 
+let browser;
 // fetch all members from guild
-client.once('ready', () => {
+client.once('ready', async () => {
+  browser = await puppeteer.launch();
   console.log(`Logged in as ${client.user.tag}!`);
   client.guilds.cache.get('1021506523140202618').members.fetch();
 });
 
+async function screenshotPage(url, file) {
+  const page = await browser.newPage();
+  await page.emulate(puppeteer.devices['iPad (gen 7)']);
+  await page.goto(url);
+  await page.screenshot({path: file, fullPage: true});
+  await page.close();
+}
+
+async function rechercheMot(messageContent) {
+  messageContent = messageContent.replace("!rechercheMot ", "");
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 1920,
+    height: 1080,
+    deviceScaleFactor: 1,
+  });
+  await page.goto("https://unifions.tonyguan3.repl.co/index.html");
+  await page.waitForSelector('#searchInput');
+  await page.$eval('#searchInput', (el, val) => el.value = val, messageContent);
+  await page.screenshot({path: "./recherchetest.png", fullPage: true});
+  await page.click('#searchButton');
+  await page.waitForTimeout(200);
+
+  await page.screenshot({path: "./rechercheMot.png", fullPage: true});
+  await page.close();
+}
+
 client.on('messageCreate', async (message) => {
-    // make a command that posts an image of a random cat
-    if (message.content === '!cat') {
-        // get a random cat from the cat api
-        fetch('http://random.cat/meow')
-            .then(response => response.json())
-            .then(data => {
-                // send the cat image
-                message.channel.send("test", { attachments: [data.file] });
-            });
-    }
-
-  // recupere moi la position d'une div en javascript
-  if (message.content === '!position') {
-    message.channel.send("test", { files: ["./position.png"] });
+  switch (message.content) {
+    case '!membres':
+      message.channel.send(`Il y a ${client.guilds.cache.get('1021506523140202618').memberCount} membres sur le serveur`);
+      break;
+    case '!bonjour':
+      message.channel.send(`Bonjour ${message.author.username}`);
+      break;
+    case '!recherche':
+      await screenshotPage("https://soup.kagescan.fr", "full.png");
+      message.channel.send({ files: ["./full.png"] });
+      break;
+    case '!histoire':
+      await screenshotPage("https://unifions.tonyguan3.repl.co/histoire.html", "histoire.png");
+      message.channel.send({ files: ["./histoire.png"] });
+      break;
+    case '!easteregg':
+      await screenshotPage("https://th.bing.com/th/id/OIP.8wLhqVonagL-Je3R5emV2AHaM0?w=189&h=328&c=7&r=0&o=5&pid=1.7", "easteregg.png");
+      message.channel.send({ files: ["./easteregg.png"] });
+      break;      
   }
 
-  // faire une fonction qui affiche le nombre de membres dans un serveur 
-  if (message.content === '!membres') {
-    message.channel.send(`Il y a ${client.guilds.cache.get('1021506523140202618').memberCount} membres sur le serveur`);
+  if (message.content.startsWith("!rechercheMot ")) {
+    await rechercheMot(message.content);
+    message.channel.send({ files: ["./rechercheMot.png"] });
   }
-  // faire une fonction qui affiche bonjour 
-  if (message.content === '!bonjour') {
-    message.channel.send(`Bonjour ${message.author.username}`);
-  }
-
-  // faire une commande qui donne le lien de cette page
-  if (message.content === '!bot') {
-    message.channel.send("https://discord.com/channels/1021506523140202618/1047986711856169020", { files: ["./index.html"] });
-  }
-
-  if (message.content === '!recherche') {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.emulate(puppeteer.devices['iPad (gen 7)']);
-    await page.goto('https://soup.kagescan.fr');
-    await page.screenshot({path: 'full.png', fullPage: true});
-    await browser.close();
-    // upload the full.png in the channel
-    message.channel.send({ files: ["./full.png"] });
-  }  
-  if (message.content === '!youtube') {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.emulate(puppeteer.devices['iPad (gen 7)']);
-    await page.goto('https://youtube.com');
-    await page.screenshot({path: 'p.png', fullPage: true});
-    await browser.close();
-    // upload the full.png in the channel
-    message.channel.send({ files: ["./p.png"] });
-  }  
-  // fait moi une fonction qui me donne le nombre de membres sur le serveur
-  if (message.content === '!membres') {
-    message.channel.send(`Il y a ${client.guilds.cache.get('1021506523140202618').memberCount} membres sur le serveur`);
-  }
-  if (message.content === '!histoire') {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.emulate(puppeteer.devices['iPad (gen 7)']);
-    await page.goto('https://unifions.tonyguan3.repl.co/histoire.html');
-    await page.screenshot({path: 'histoire.png', fullPage: true});
-    await browser.close();
-    // upload the full.png in the channel
-    message.channel.send({ files: ["./histoire.png"] });
-  }  
-  if (message.content === '!ph') {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.emulate(puppeteer.devices['iPad (gen 7)']);
-    await page.goto('https://www.pornhub.fr');
-    await page.screenshot({path: 'ph.png', fullPage: true});
-    await browser.close();
-    // upload the full.png in the channel
-    message.channel.send({ files: ["./ph.png"] });
-  }  
-  if (message.content === '!easteregg') {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.emulate(puppeteer.devices['iPad (gen 7)']);
-    await page.goto('https://th.bing.com/th/id/OIP.8wLhqVonagL-Je3R5emV2AHaM0?w=189&h=328&c=7&r=0&o=5&pid=1.7');
-    await page.screenshot({path: 'easteregg.png', fullPage: true});
-    await browser.close();
-    // upload the full.png in the channel
-    message.channel.send({ files: ["./easteregg.png"] });
-  }  
-  
-  
 })
 
 
